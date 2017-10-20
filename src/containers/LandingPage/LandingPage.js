d919515f36d6f3b229c153464b0bb0c738d8367f
Michael Liou <michaelliou@Michael-Liou-MacBook-Pro.local>
2017-10-20 15:51:25 -0400

    update to moving animations

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react'
 import Message from 'components/Message/Message.js'
-import { CSSTransitionGroup } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
-const messageArr = [
+const messageArrOne = [
   { msg: 'Hey there, I\'m Michael!' },
   { msg: 'I built a tool that helped people who have similar goals and interests connect' },
   { msg: 'nothing more nothing less!' },
@@ -25,17 +25,38 @@ const messageArrTwo = [
   }
 ]
 
+const SlideIn = ({ children, ...props }) => (
+  <CSSTransition
+    {...props}
+    timeout={1000}
+    classNames="slide"
+  >
+    {children}
+  </CSSTransition>
+);
+
+
 class LandingPage extends Component {
   constructor(props) {
     super(props)
+    this.renderMessage = this.renderMessage.bind(this);
+    this.state = { activeMessage: messageArrOne}
+  }
+
+  renderMessage(messageArr) {
+    return messageArr.map((message) => {
+      return (
+        <Message
+          messageContent={message.msg}
+        />
+      )
+    })
   }
 
   render() {
     return (
       <div>
-        <Message
-          messageContent={messageArr[0].msg}
-        />
+        {this.renderMessage(this.state.activeMessage)}
       </div>
     )
   }
--- todos.md
+++ todos.md
@@ -4,8 +4,13 @@ Build out the initial dialogue page / Landing page
 
 -- First step. Need to figure out a timed release of each chat box followed by a yes / no dialogue option.
 
+
 Architecture:
 
-Build some styled components that take in each chat
+-- Build some styled components that take in each chat
+-- Landing Page Component
+** Message components load out from a message Array
+** Each decision point loads in a different message array
+
 
 https://azumbrunnen.me/
